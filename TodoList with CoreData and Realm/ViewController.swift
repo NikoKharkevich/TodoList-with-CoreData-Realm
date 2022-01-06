4c8d7e0b9f9ab7c6d127b1e1ead32d8196324260
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-01-06 15:58:17 +0200

    Initial Commit

---
+++ TodoList with CoreData and Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList with CoreData and Realm
+//
+//  Created by Nikola Kharkevich on 06.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
